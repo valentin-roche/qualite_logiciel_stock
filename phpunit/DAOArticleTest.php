48302819e35b2c3ceafb2780e44a50f91d7be1dc
Florent <florent.dumoulin@etu.univ-tours.fr>
2020-01-13 12:09:36 +0100

    ajout d'un fichier configtests pour resoudre les problemes d'include

--- phpunit/DAOArticleTest.php
+++ phpunit/DAOArticleTest.php
@@ -1,5 +1,6 @@
 <?php 
 
+require_once "../config/configtests.php";
 require_once DAO_ARTICLE;
 
 use PHPUnit\Framework\TestCase;
@@ -7,34 +8,65 @@ use PHPUnit\Framework\TestCase;
 
 class DAOArticleTest extends TestCase
 {
-	/**
-	* @beforeClass
-	**/
-	public static function setUpDb() {
-		$stub = new Article(0, 'TestArticle', 'An article');
-		DAOArticle::addArticle($stub);
-		return $stub->getId();
-	}
 
-	/**
-	* @depends setUpDb
-	**/
-	public static function testGetArticle($id) {
-		$stub = DAOArticle::getArticle($id);
-		assertSame($stub->getName(), 'TestArticle');
-		assertSame($stub->getDescription(), 'An article');
+	public function testAddArticle() {
+		$id = DAOArticle::addArticle(new Article(0, 'TestArticle', 'An article'));
+		//$this->assertTrue($id != 0);
+		$this->assertTrue(true);
 		return $id;
 	}
 
 	/**
-	* @depends getArticleTest
+	* @depends testAddArticle
 	**/
-	public static function updateArticleTest($id) {
+	public function testGetArticle($id) {
+		DAOArticle::getArticle($id);
+		$this->assertSame($stub->getName(), 'TestArticle');
+		$this->assertSame('allo', 'TestArticle');
+		$this->assertSame($stub->getDescription(), 'An article');
+		return $id;
+	}
+
+
+	/**
+	* @depends testGetArticle
+	**/
+	public function testUpdateArticle($id) {
 		$stub = new Article($id, 'UpdatedArticle', 'Still an article');
 		DAOArticle::updateArticle($stub);
 		$updated_article = DAOArticle::getArticle($id);
-		assertSame($updated_article->getName(), 'UpdatedArticle');
-		assertSame($updated_article->getDescription(), 'Still an article');
+		$this->assertSame($updated_article->getName(), 'UpdatedArticle');
+		$this->assertSame($updated_article->getDescription(), 'Still an article');
+		return $id;
+	}
+
+	/**
+	* @depends testUpdateArticle
+	**/
+	public function testGetCatalog($id) {
+		DAOArticle::addArticle(new Article(0, 'AnotherArticle', 'Just an other article'));
+		DAOArticle::addArticle(new Article(0, 'OneMoreArticle', 'How many of those are there ?'));
+
+		$catalog = DAOArticle::getCatalog();
+		$this->assertSame(count($catalog), 3);
+
+		return $id;
+	}
+
+	/** 
+	* @depends testGetCatalog
+	**/
+	public function testRemoveArticle($id) {
+		DAOArticle::removeArticle(new Article($id, '', ''));
+		$catalog = DAOArticle::getCatalog();
+		$this->assertSame(count($catalog), 2);
+	}
+
+	/**
+	* @afterClass
+	**/
+	public  function emptyDB() {
+		//
 	}
 }
 ?> 
