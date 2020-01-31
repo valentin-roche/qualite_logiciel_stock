67bd605c0d00527cd588df824f387df4542cb9a2
valentin-roche <35733063+valentin-roche@users.noreply.github.com>
2020-01-31 23:29:06 +0100

    ajout des fonctions crypt et decrypt

--- model/User.php
+++ model/User.php
@@ -143,4 +143,35 @@ class User
     {
         $this->idRayon = $idRayon;
     }
+
+    public static function cryptPass($pwd) {
+      $encrypt_method = "AES-256-CBC";
+      $secret_key = 'e9058ab198f6908f702111b0c0fb5b36f99d00554521886c40e2891b349dc7a1';
+      $secret_iv = '17550e2bb9ff2c26dcce8ed178e326202cc9c67f16b79470767f01839a062249';
+
+      // hash
+      $key = hash('sha256', $secret_key);
+
+      //iv encryption
+      $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+      $output = openssl_encrypt($password, $encrypt_method, $key, 0, $iv);
+      $output = base64_encode($output);
+      return $output;
+    }
+
+    public static function decryptPass($pwd) {
+      $encrypt_method = "AES-256-CBC";
+      $secret_key = 'e9058ab198f6908f702111b0c0fb5b36f99d00554521886c40e2891b349dc7a1';
+      $secret_iv = '17550e2bb9ff2c26dcce8ed178e326202cc9c67f16b79470767f01839a062249';
+
+      // hash
+      $key = hash('sha256', $secret_key);
+
+      //iv encryption
+      $iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+      $output = openssl_decrypt(base64_decode($password), $encrypt_method, $key, 0, $iv);
+      return $output;
+    }
 }
