00830ef702d3b9d1904088edff1aa996b1586c99
valentin-roche <35733063+valentin-roche@users.noreply.github.com>
2020-02-01 19:02:25 +0100

    hotfix et ajout vidéos doc

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ require_once "header.php";
 require_once DAO_ARTICLE;
 
 displayHeader("Accueil", "");
+if (isset($_SESSION['name'])) {
 ?>
 
 <a href="addArticle.php?navAction=addArticle">Ajouter un article</a>
+<?php } ?>
 <h2>Catalogue :</h2>
 <table>
   <tbody>
