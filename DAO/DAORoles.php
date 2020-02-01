9c17888e1f1aac0cf19f61c248356bb8c9611613
valentin-roche <35733063+valentin-roche@users.noreply.github.com>
2020-02-01 11:44:09 +0100

    ajout méthode getRoles

--- DAO/DAORoles.php
+++ DAO/DAORoles.php
@@ -9,6 +9,21 @@ class DAORoles
 {
   function __construct() {}
 
+  public static function getRoles() {
+    $db = ConnectBDD::getConnection();
+
+    $req = $db->prepare('SELECT * FROM role');
+
+    $req->execute();
+    $role_list = [];
+    $roleDataList = $req->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($roleDataList as $roleData) {
+        $role = new Role();
+        $role->create($roleData['idRole'], $roleData['libelle']);
+        $role_list[] = $role;
+    }
+    return $role_list;
+  }
 
 }
 
